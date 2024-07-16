bcbec61b1c3d529e6a9d4a547cc0463f2b2ed0d2
KimDabomi <cdabomi@nate.com>
2024-07-17 03:15:15 +0900

    section0 complete

--- Nodejs-master/main.js
+++ Nodejs-master/main.js
@@ -7,23 +7,23 @@ const compression = require('compression');
 const helmet = require('helmet')
 
 app.use(helmet());
- 
+
 const indexRouter = require('./routes/index');
 const topicRouter = require('./routes/topic');
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(compression());
-app.get('*', function(request, response, next){
-  fs.readdir('./data', function(error, filelist){
+app.get('*', function (request, response, next) {
+  fs.readdir('./data', function (error, filelist) {
     request.list = filelist;
     next();
   });
 });
- 
+
 app.use('/', indexRouter);
 app.use('/topic', topicRouter);
- 
+
 app.use((req, res, next) => {
   res.status(404).send('Sorry cant find that!');
 })
--- Nodejs-master/routes/index.js
+++ Nodejs-master/routes/index.js
@@ -3,18 +3,18 @@ const router = express.Router();
 const template = require('../lib/template.js');
 
 router.get('/', function (request, response) {
-    const title = 'Welcome';
-    const description = 'Hello, Node.js';
-    const list = template.list(request.list);
-    const html = template.HTML(title, list,
-        `
+  const title = 'Welcome';
+  const description = 'Hello, Node.js';
+  const list = template.list(request.list);
+  const html = template.HTML(title, list,
+    `
       <h2>${title}</h2>${description}
       <img src="/images/hello.jpg" style="width:300px; display:block; margin-top:10px;">
       `,
-        `<a href="/topic/create">create</a>`
-    );
+    `<a href="/topic/create">create</a>`
+  );
 
-    response.send(html);
+  response.send(html);
 });
 
 module.exports = router;
