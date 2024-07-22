de6a2818aee8efb51259e494a67c6a09fe2b8ad1
KimDabomi <cdabomi@nate.com>
2024-07-22 17:34:53 +0900

    day3

--- Nodejs-master/nodejs/cookie.js
+++ Nodejs-master/nodejs/cookie.js
@@ -12,7 +12,15 @@ http.createServer((req, res) => {
     console.log("yummy_cookie", cookies.yummy_cookie);
 
     res.writeHead(200, {
-        'Set-Cookie': ['yummy_cookie=choco', 'tasty_cookie=strawberry']
+        'Set-Cookie': [
+            'yummy_cookie=choco', 
+            'tasty_cookie=strawberry', 
+            `Permanent=cookies; Max-Age=${60*60*24*30}`,
+            'Secure=Secure; Secure',
+            'HttpOnly=HttpOnly; HttpOnly',
+            'Path=Path; Path=/cookie',
+            'Domain=Domain; Domain=o2.org'
+        ]
     });
 
     res.end('Cookie!');
